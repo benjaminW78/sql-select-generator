decb35ce88a795eb30555c1f755139ff322f4568
mrobin <mrobin@companeo.com>
2016-07-13 10:10:14 +0200

    adding code corresponding to from method

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-/*globals module */
+/*globals module, console */
 
-module.exports = {};
+module.exports = function () {
+    'use strict';
+
+    const
+        errorPrefix = '[sqlSelect] Error : ',
+        errorBadArguments = errorPrefix + 'Bad arguments.',
+        description = {};
+
+    return {
+        from: function (origin) {
+            const
+                localErrorBadArguments = errorBadArguments + ' #from() need a non-empty string argument or object with label property at least.',
+                result = {};
+
+            if ('string' === typeof origin && '' !== origin.trim()) {
+                result.label = origin.trim();
+            } else if ('object' === typeof origin) {
+                if ('string' === typeof origin.label && '' !== origin.label.trim()) {
+                    result.label = origin.label.trim();
+                    if (undefined !== origin.alias) {
+                        result.alias = origin.alias;
+                    }
+                } else {
+                    throw localErrorBadArguments;
+                }
+            } else {
+                throw localErrorBadArguments;
+            }
+
+            description.from = result;
+
+            return this;
+        },
+        join: function () {
+
+        },
+        columns: function () {
+
+        },
+        order: function () {
+
+        },
+        toString: function () {
+
+        }
+    };
+};
